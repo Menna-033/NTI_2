113d9463c6be9a2108b53136bd85d9e30d35c2f4
Menna-033 <yassermenna033@gmail.com>
2026-09-08 12:40:49 +0300

    Add files via upload

---
+++ headers/ADC3.h
@@ -0,0 +1,9 @@
+#ifndef ADC3_H
+#define ADC3_H
+
+#include "STD_TYPES.h"
+
+void ADC_voidInit(void);
+u16  ADC_u16ReadChannel(u8 Copy_u8Channel);
+
+#endif
---
+++ headers/ADC4.h
@@ -0,0 +1,9 @@
+#ifndef ADC_H
+#define ADC_H
+
+#include "STD_TYPES.h"
+
+void ADC_voidInit(void);
+u16  ADC_u16ReadChannel(u8 Copy_u8Channel);
+
+#endif
